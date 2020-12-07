190ee28268d21e10226ae495850db3d4c99720b7
matheus3006 <matheussouza18@live.com>
2020-12-07 12:39:28 -0300

    .

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 //Middleware
 const morgan = require('morgan');
-
-
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+//DB
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app=express();
 
@@ -19,9 +23,16 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+const server = app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhadled primise rejections
+process.on('unhandledRejection', (err,promise)=>{
+    console.log(`Error: ${err.message}`.red.bold);
+    //Close  server & exit process
+    server.close(()=> process.exit(1));
+})
