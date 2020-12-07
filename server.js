9fa68678f1227395c1ef9a20a9667f6e0b18bd3f
matheus3006 <matheussouza18@live.com>
2020-12-06 21:15:13 -0300

    init files

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//Middleware
+const morgan = require('morgan');
+
+
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 const app=express();
+
+// Dev loggin middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000
 
