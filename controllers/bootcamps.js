305b96f273f90cfdb7ce5dc089995dab3d75612c
matheus3006 <matheussouza18@live.com>
2020-12-11 11:12:10 -0300

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,10 +13,12 @@ const asyncHandler = require('../middleware/async');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
         let query;
         //Copy req.query
-        const reqQuery={...req.query};
+        const reqQuery = {
+                ...req.query
+        };
 
         //Fields to exclude
-        const removeFields = ['select','sort'];
+        const removeFields = ['select', 'sort', 'page', 'limit'];
 
         // Loop over remove Fields and delete them from reqQuery
         removeFields.forEach(param => delete reqQuery[param]);
@@ -25,29 +27,57 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         let queryStr = JSON.stringify(reqQuery);
 
         //Create Operetor ($gt, $gte , etc...);
-        queryStr =  queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
         //Finding resource
         query = Bootcamp.find(JSON.parse(queryStr));
 
         // Select Fields
-        if(req.query.select){
+        if (req.query.select) {
                 const fields = req.query.select.split(',').join(' ');
                 query = query.select(fields);
         }
 
         // Sort
-        if(req.query.sort){
+        if (req.query.sort) {
                 const sortBy = req.query.sort.split(',').join(' ');
                 query = query.sort(sortBy);
-        }else{
+        } else {
                 query = query.sort('-createdAt');
         }
 
+        // Pagination
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await Bootcamp.countDocuments();
+     
+
+        query = query.skip(startIndex).limit(limit);
+
         // Executing query
         const bootcamps = await query;
+
+        // Pagination Result
+        const pagination = {};
+
+        if (endIndex < total) {
+                pagination.next = {
+                        page: page + 1,
+                        limit
+                };
+        }
+        if (startIndex > 0) {
+                pagination.prev = {
+                        page: page - 1,
+                        limit
+                };
+        }
+
         res.status(200).json({
                 success: true,
+                pagination,
                 count: bootcamps.length,
                 data: bootcamps
         });
@@ -118,25 +148,34 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 // @route   GET /api/v1/bootcamps/:zipcode/:distance
 // @access  Private
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-        const { zipcode, distance } = req.params;
-      
+        const {
+                zipcode,
+                distance
+        } = req.params;
+
         // Get lat/lng from geocoder
         const loc = await geocoder.geocode(zipcode);
         const lat = loc[0].latitude;
         const lng = loc[0].longitude;
-      
+
         // Calc radius using radians
         // Divide dist by radius of Earth
         // Earth Radius = 3,963 mi / 6,378 km
         const radius = distance / 3963;
-      
+
         const bootcamps = await Bootcamp.find({
-          location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+                location: {
+                        $geoWithin: {
+                                $centerSphere: [
+                                        [lng, lat], radius
+                                ]
+                        }
+                }
         });
-      
+
         res.status(200).json({
-          success: true,
-          count: bootcamps.length,
-          data: bootcamps
+                success: true,
+                count: bootcamps.length,
+                data: bootcamps
         });
-      });
+});
