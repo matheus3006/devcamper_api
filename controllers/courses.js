68f72e852acc48d8927f16fa582ffe5b00231c8a
matheus3006 <matheussouza18@live.com>
2020-12-13 19:04:31 -0300

    delete/update and avarageCost add

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
@@ -9,13 +10,15 @@ const asyncHandler = require('../middleware/async');
 // @route   GET /api/v1/courses
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
-exports.getCourses = asyncHandler(async(req,res,next)=>{
+exports.getCourses = asyncHandler(async (req, res, next) => {
     let query;
 
-    if(req.params.bootcampId){
-        query = Course.find({bootcamp: req.params.bootcampId});
-    }else{
-        query= Course.find().populate({
+    if (req.params.bootcampId) {
+        query = Course.find({
+            bootcamp: req.params.bootcampId
+        });
+    } else {
+        query = Course.find().populate({
             path: 'bootcamp',
             select: 'name description'
         });
@@ -28,4 +31,105 @@ exports.getCourses = asyncHandler(async(req,res,next)=>{
         count: courses.length,
         data: courses
     });
+});
+
+// @desc    Get courses
+// @route   GET /api/v1/courses/:id
+// @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+// @desc    Add courses
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(
+                `No bootcamp with the id of ${req.params.bootcampId}`,
+                404
+            )
+        );
+    }
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+
+
+});
+
+// @desc    Update courses
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(
+                `No course with the id of ${req.params.id}`,
+                404
+            )
+        );
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+
+
+});
+
+// @desc    delete courses
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(
+                `No course with the id of ${req.params.id}`,
+                404
+            )
+        );
+    }
+
+    await course.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {},
+        msg:"Course deleted"
+    });
+
+
 });
--- models/Course.js
+++ models/Course.js
@@ -2,42 +2,83 @@ const mongoose  = require('mongoose');
 const Bootcamp = require('./Bootcamp');
 
 const CourseSchema = new mongoose.Schema({
-    title:{
+    title: {
         type: String,
         trim: true,
-        required: [true,'please add a course title']
-    },
-    description:{
+        required: [true, 'Please add a course title']
+      },
+      description: {
         type: String,
-        required:[true, 'Please add a description']
-    },
-    weeks:{
+        required: [true, 'Please add a description']
+      },
+      weeks: {
         type: String,
-        required:[true, 'Please add number of weeks']
-    },
-    tuition:{
+        required: [true, 'Please add number of weeks']
+      },
+      tuition: {
         type: Number,
-        required:[true, 'Please add a tuition cost']
-    },
-    minimumSkill:{
+        required: [true, 'Please add a tuition cost']
+      },
+      minimumSkill: {
         type: String,
-        required:[true, 'Please add a minimum  skill '],
-        enum:['beginner', 'intermediate', 'advanced']
-    },
-    scholarshipAvaible:{
+        required: [true, 'Please add a minimum skill'],
+        enum: ['beginner', 'intermediate', 'advanced']
+      },
+      scholarshipAvailable: {
         type: Boolean,
         default: false
-    },
-    createAt:{
+      },
+      createdAt: {
         type: Date,
         default: Date.now
-    },
-    bootcamp:{
+      },
+      bootcamp: {
         type: mongoose.Schema.ObjectId,
-        ref:'Bootcamp',
+        ref: 'Bootcamp',
         required: true
-    }
+      }
 
 });
 
-module.exports = mongoose.model('Course' , CourseSchema);
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+    const obj = await this.aggregate([
+      {
+        $match: { bootcamp: bootcampId }
+      },
+      {
+        $group: {
+          _id: '$bootcamp',
+          averageCost: { $avg: '$tuition' }
+        }
+      }
+    ]);
+  
+    try {
+   if (obj[0]) {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+          averageCost:Math.ceil(obj[0].averageCost / 10) * 10,
+        });
+      } else {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+          averageCost: undefined,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    console.log(Math.ceil(obj[0].averageCost/10)*10);
+  };
+  
+  // Call getAverageCost after save
+  CourseSchema.post('save', async function() {
+    await this.constructor.getAverageCost(this.bootcamp);
+  });
+  
+  // Call getAverageCost after remove
+  CourseSchema.post('remove', async function () {
+    await this.constructor.getAverageCost(this.bootcamp);
+  });
+
+
+module.exports = mongoose.model('Course', CourseSchema);
