459bf8f54f52a60ff52a34df612c3dd807646b8c
matheus3006 <matheussouza18@live.com>
2020-12-22 19:10:08 -0300

    register and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,11 +3,61 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc      Register User
-// @route     GET /api/v1/auth/register
+// @route     POST /api/v1/auth/register
 // @access    Public
 exports.register =  asyncHandler(async(req,res,next)=>{
-    res.status(200).json({
-        success:true
-    })
+   const {
+    name,email,password,role
+   } = req.body
 
+//    Create user
+const user = await User.create({
+    name,
+    email,
+    password,
+    role
 });
+
+//Create TOken
+const token = user.getSingedJwtToken();
+    
+res.status(200).json({
+    success: true,
+    token
+});
+});
+
+
+// @desc      Login User
+// @route    Post /api/v1/auth/login
+// @access    Public
+exports.login =  asyncHandler(async(req,res,next)=>{
+    const {
+    email
+    ,password } = req.body
+ 
+// Validete email & password
+if(!email || !password){
+    return next(new ErrorResponse('Please provide an email and password', 400));
+}
+
+// Check for user
+const user = await User.findOne({email}).select('+password');
+
+if(!user){
+    return next(new ErrorResponse('Invalid credentials', 401));
+}
+
+// Check if password matches
+const isMatch = await user.mathPassword(password);
+
+if(!isMatch){
+    return next(new ErrorResponse('Invalid credentials', 401));
+}
+ 
+    
+ res.status(200).json({
+     success: true,
+     token
+ });
+ });
